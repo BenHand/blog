32c47dfc69670188141c6f02ed4c97fa2258d3f2
Benjamin Hand <gneeshot@gmail.com>
2015-06-09 14:39:27 -0500

    added partial for displaying blog posts and set up create method

--- app/controllers/blog_post_controller.rb
+++ app/controllers/blog_post_controller.rb
@@ -1,7 +1,27 @@
 class BlogPostController < ApplicationController
 
   def index
-    render 'index.html.erb', locals: { blog_posts: BlogPost.all }
+    all_posts = BlogPost.all
+    render 'index.html.erb', locals: { blog_posts: all_posts }
+  end
+
+  def show
+    render 'show.html.erb', locals: { blog_posts: BlogPost.find(params[:id]) }
+  end
+
+  def new
+    render 'new.html.erb', locals: { blog_posts: BlogPost.new }
+  end
+
+  def create
+    new_post = BlogPost.create(blog_post_params)
+    render 'show.html.erb', locals: { blog_posts: new_post }
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_posts).permit(:title, :body)
   end
 
 end
